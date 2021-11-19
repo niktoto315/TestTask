8e2998c098e8001a6a75e062718440f313367eac
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2021-11-19 17:01:56 +0700

    Add files via upload

---
+++ Scripts/Score.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int PlayerScore;
+    public static int EnemyScore;
+    public Text ScoreText;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        ScoreText.text = $"Kills: {PlayerScore} : {EnemyScore}";
+    }
+}
